c11cfb822bbc2dff92cf2675c43fb766a918b0b0
ArtAmmirati <34355879+ArtAmmirati@users.noreply.github.com>
2018-01-10 13:04:49 -0500

    Add files via upload
    Three conversion udfunctions

---
+++ Conversions.sql
@@ -0,0 +1,20 @@
+	CREATE FUNCTION [ufn.in2cm] (@in decimal(10,3))
+	RETURNS decimal(10,3)
+	AS
+	BEGIN
+	RETURN (@in * 2.54)
+	END
+	GO
+	CREATE FUNCTION [ufn.gal2lit] (@gal decimal(10,3))
+	RETURNS decimal(10,3)
+	AS
+	BEGIN
+	RETURN (@gal * 3.785)
+	END
+	GO
+	CREATE FUNCTION [ufn.lbs2kg] (@lbs decimal(10,3))
+	RETURNS decimal(10,3)
+	AS
+	BEGIN
+	RETURN (@lbs * 2.54)
+	END
